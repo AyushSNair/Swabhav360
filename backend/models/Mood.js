1ac6db265244b4f066566fb8e417f821a5023e3d
Aryan <nairaryan135@gmail.com>
2025-06-19 10:46:18 +0530

    update

--- backend/models/Mood.js
+++ backend/models/Mood.js
@@ -1,12 +1,15 @@
-// backend/models/Mood.js
 import mongoose from 'mongoose';
 
 const moodSchema = new mongoose.Schema({
-  uid: { type: String, required: true },  // Firebase UID
+  uid: { type: String, required: true },
+  date: { type: String, required: true }, // ISO string (e.g., '2025-06-19')
   mood: { type: String, required: true },
-  note: { type: String },
-  intensity: { type: Number, default: 3 },  // Add this line
-  date: { type: Date, default: Date.now }
+  intensity: { type: Number, required: true },
+  note: { type: String }
 });
 
+// Ensure only 1 entry per user per date
+moodSchema.index({ userId: 1, date: 1 }, { unique: true });
+
+// module.exports = mongoose.model('Mood', moodSchema);
 export default mongoose.model('Mood', moodSchema);
